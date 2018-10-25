1b133f4adadbc57096b5cd698a1eb5ca23a256f3
plinck <paul.linck@gmail.com>
2018-10-25 15:01:12 -0400

    trying to add sound

--- Just Dice/Controller/ViewController.swift
+++ Just Dice/Controller/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
@@ -50,9 +51,33 @@ class ViewController: UIViewController {
         imageView.animationDuration = 1.0
         imageView.animationRepeatCount = 1
         imageView.startAnimating()
+        
+        UIView.animate(withDuration: 1.0, animations: {
+            () -> Void in
+            let rotation1: Int = Int.random(in: 180...360)
+            let rotation2: Int = -rotation1
+            
+            // Rotate and scale - must use concatention to do multiple transforms
+            var tt1 = CGAffineTransform(rotationAngle: CGFloat(rotation1))
+            tt1 = tt1.concatenating(CGAffineTransform(scaleX: 5, y: 5))
+            self.diceImageView1.transform = tt1
+            
+            var tt2 = CGAffineTransform(rotationAngle: CGFloat(rotation2))
+            tt2 = tt2.concatenating(CGAffineTransform(scaleX: 2.0, y: 2.0))
+            self.diceImageView2.transform = tt2
+
+        })
+    }
+    
+    func resetAnimate() {
+        self.diceImageView1.transform = CGAffineTransform(rotationAngle: CGFloat(0))
+        self.diceImageView2.transform = CGAffineTransform(rotationAngle: CGFloat(0))
+
     }
     
     func pickImages() {
+        
+        self.playSound()
         animate(imageView: diceImageView1, images: diceImages)
         randomDiceIndex1 = Int.random(in: 0...5)
         diceImageView1.image = diceImages[randomDiceIndex1]
@@ -60,6 +85,38 @@ class ViewController: UIViewController {
         animate(imageView: diceImageView2, images: diceImages)
         randomDiceIndex2 = Int.random(in: 0...5)
         diceImageView2.image = diceImages[randomDiceIndex2]
+        
+        resetAnimate()
     }
 }
 
+// MARK: -
+// Added audio in here for now
+extension ViewController {
+    
+    func playSound() {
+        var player: AVAudioPlayer?
+
+      guard let soundAsset = NSDataAsset(name: "ShakeRoll") else { return }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playAndRecord, mode: .default, options: [])
+            try AVAudioSession.sharedInstance().setActive(true)
+                        
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            // player = try AVAudioPlayer(contentsOf: soundAsset, fileTypeHint: AVFileType.mp3.rawValue)
+            player = try AVAudioPlayer(data:soundAsset.data)
+            
+            /* iOS 10 and earlier require the following line:
+             player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+            
+            guard let player = player else { return }
+            
+            player.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+
+}
